97249d4533c583e68a51921bc8ee346d9c6e65e9
Suhas <suhpin95@gmail.com>
2021-03-16 12:40:42 +0530

    1. Implemented subscription by using websockets 2. Used nodemon as dev dependencies

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const { GraphQLServer } = require('graphql-yoga');
+const { GraphQLServer, PubSub } = require('graphql-yoga');
 
 const messages = [];
 const typeDefs = `
@@ -9,13 +9,21 @@ const typeDefs = `
     }
 
     type Query{
-        messages: [Message]!
+        messages: [Message!]
     }
 
     type Mutation{
         postMessage(user: String!, content: String!): ID!
     }
+
+    type Subscription{
+        messages: [Message!]
+    }
 `; 
+
+const pubsub = new PubSub();
+const subscribers = [];
+const onMessagesUpdates = (fn) => subscribers.push(fn);
 const resolvers = {
     Query:{
         messages: ()=> messages,
@@ -28,11 +36,22 @@ const resolvers = {
                 user,
                 content
             })
+            subscribers.forEach((fn) => fn());
             return id;
+        }
+    },
+    Subscription: {
+        messages: {
+            subscribe: (parent, args, { pubsub })=> {
+                const channel = Math.random().toString(36).slice(2,15);
+                onMessagesUpdates(()=> pubsub.publish(channel, { messages }))
+                setTimeout(()=> pubsub.publish(channel, { messages }), 0);
+                return pubsub.asyncIterator(channel)
+            },
         }
     }
 }
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
 
 server.start(({ port }) => {
     console.log(`Server on http://localhost:${port}/`);
